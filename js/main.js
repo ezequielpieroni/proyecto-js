5f42388667ca24a4032fa0773289548e06aecb23
ezequielpieroni <ezequielpieroni@gmail.com>
2024-02-03 22:39:46 +0100

    Proyecto JS entrega 1.1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 let usuario = "Eze";
 let pass = "123456";
-let resultado;
 
 function logIn() {
      for (let i=0; i<3; i++){
         let ingresoUser = prompt("Ingrese usuario");
         let ingresoPass = prompt("Ingrese contraseña"); 
-        if (ingresoUser == usuario && ingresoPass == pass ) {
+        if (ingresoUser === usuario && ingresoPass === pass ) {
             return true;
         } else {
             alert("Usuario o contraseña incorrecta");
@@ -15,9 +14,8 @@ function logIn() {
     return false;
 }
 
-resultado = logIn();
 
-if (resultado) {
+if (logIn()) {
     alert ("Bienvenido/a")
 } else {
     alert ("Has superado el numero de intentos")
